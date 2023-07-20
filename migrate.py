964db3d51807ab483f00e425910ea5c3c57d7f6e
Fernando D. Gómez <fgomezdev@gmail.com>
2023-07-20 09:55:17 -0300

    Se ajustó la verificación de tabla existente en destino

--- migrate.py
+++ migrate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-""" Script para migrar la estructura y los datos de una base de datos a otra."""
+"""Script para migrar la estructura y los datos de una base de datos a otra."""
 
 
 import json
@@ -8,41 +8,41 @@ import psycopg2
 from datetime import datetime
 from decimal import Decimal
 
-SOURCE_HOST = "localhost"
+SOURCE_HOST = 'localhost'
 SOURCE_PORT = 5432
-SOURCE_DB = "demo"
-SOURCE_USER = "postgres"
-SOURCE_PASS = "postgres"
-SOURCE_SCHEMA = "bookings"
+SOURCE_DB = 'demo'
+SOURCE_USER = 'postgres'
+SOURCE_PASS = 'postgres'
+SOURCE_SCHEMA = 'bookings'
 
-TARGET_HOST = "localhost"
+TARGET_HOST = 'localhost'
 TARGET_PORT = 5432
-TARGET_DB = "demo"
-TARGET_USER = "postgres"
-TARGET_PASS = "postgres"
-TARGET_SCHEMA = "bookings_new"
+TARGET_DB = 'demo'
+TARGET_USER = 'postgres'
+TARGET_PASS = 'postgres'
+TARGET_SCHEMA = 'bookings_new'
 
-tables_to_migrate = [    
-    "aircrafts_data",
-    "airports_data",
-    "boarding_passes",
-    "bookings",
-    "flights",
-    "seats",    
-    "ticket_flights",
-    "tickets",
+tables_to_migrate = [
+    'aircrafts_data',
+    'airports_data',
+    'boarding_passes',
+    'bookings',
+    'flights',
+    'seats',
+    'ticket_flights',
+    'tickets',
 ]
 
 
-def migrate_table_structure(source_cursor, source_table_name, destino_cursor, target_schema, target_table_name):
+def migrate_table_structure(source_cursor, source_table_name, target_cursor, target_schema, target_table_name):
     """Replica la estructura de la tabla en la base de datos de destino."""
 
     # Verificamos si la tabla existe en la base de datos destino
-    source_cursor.execute(
+    target_cursor.execute(
         f"SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = '{target_schema}' and table_name = '{target_table_name}');"
     )
     if source_cursor.fetchone()[0]:
-        raise ValueError(f"La tabla {target_table_name} ya existe en la base de datos de destino.")
+        raise ValueError(f'La tabla {target_table_name} ya existe en la base de datos de destino.')
 
     source_cursor.execute(
         f"SELECT column_name, data_type, character_maximum_length FROM information_schema.columns WHERE table_name = '{source_table_name}' ORDER BY ordinal_position;"
@@ -50,16 +50,16 @@ def migrate_table_structure(source_cursor, source_table_name, destino_cursor, ta
     table_structure = source_cursor.fetchall()
 
     # Creamos la tabla en destino
-    sql_create_table = f"CREATE TABLE {target_schema}.{target_table_name} ("
+    sql_create_table = f'CREATE TABLE {target_schema}.{target_table_name} ('
     for column_name, data_type, character_maximum_length in table_structure:
-        sql_create_table += f"{column_name} {data_type}"
+        sql_create_table += f'{column_name} {data_type}'
         if character_maximum_length:
-            sql_create_table += f"({character_maximum_length})"
-        sql_create_table += ", "
+            sql_create_table += f'({character_maximum_length})'
+        sql_create_table += ', '
     sql_create_table = sql_create_table[:-2]  # <- Eliminamos la última coma y el espacio
-    sql_create_table += ")"
+    sql_create_table += ')'
 
-    destino_cursor.execute(sql_create_table)
+    target_cursor.execute(sql_create_table)
 
 
 def migrate_constraints(source_cursor, source_table_name, target_cursor, target_schema, target_table_name, prefix):
@@ -76,10 +76,10 @@ def migrate_constraints(source_cursor, source_table_name, target_cursor, target_
         conname, constraint_def = restriccion
         conname = conname.lower()
         constraint_def = constraint_def.replace(
-            f"{source_table_name} ", f"{target_table_name} {target_table_name.lower()} "
+            f'{source_table_name} ', f'{target_table_name} {target_table_name.lower()} '
         )
         target_cursor.execute(
-            f"ALTER TABLE {target_schema}.{target_table_name} ADD CONSTRAINT {prefix}{conname} {constraint_def}"
+            f'ALTER TABLE {target_schema}.{target_table_name} ADD CONSTRAINT {prefix}{conname} {constraint_def}'
         )
 
 
@@ -95,26 +95,27 @@ def migrates_sequences(
     columnas_secuencia = source_cursor.fetchall()
     if columnas_secuencia:
         columna_secuencia = columnas_secuencia[0][0]
-        seq_name = f"{prefix}{target_schema}.{target_table_name}_{columna_secuencia}_seq"
+        seq_name = f'{prefix}{target_schema}.{target_table_name}_{columna_secuencia}_seq'
 
         # Creamos la secuencia en la base de datos destino
-        target_cursor.execute(f"CREATE SEQUENCE {seq_name};")
+        target_cursor.execute(f'CREATE SEQUENCE {seq_name};')
         sql_sequence = (
             f"SELECT setval('{seq_name}', (SELECT max({columna_secuencia}) FROM {target_schema}.{target_table_name}))"
         )
         target_cursor.execute(sql_sequence)
 
+
 # TODO: Hacer pruebas con otros tipos de datos
 def migrate_data(source_cursor, source_schema, source_table_name, target_cursor, target_schema, target_table_name):
     """Copia los registros de la tabla de origen a la tabla de destino."""
 
-    source_cursor.execute(f"SELECT * FROM {source_schema}.{source_table_name};")
+    source_cursor.execute(f'SELECT * FROM {source_schema}.{source_table_name};')
     rows = source_cursor.fetchall()
 
     for row in rows:
         values = []
         for value in row:
-            
+
             if isinstance(value, float) or isinstance(value, int) or isinstance(value, Decimal):
                 value = str(value)
 
@@ -125,14 +126,13 @@ def migrate_data(source_cursor, source_schema, source_table_name, target_cursor,
                 value = f"'{json.dumps(value)}'"
 
             if value is None:
-                value = "null"
+                value = 'null'
 
             values.append(value)
 
-        data = ", ".join(values)
-        sql_insert = f"INSERT INTO {target_schema}.{target_table_name} VALUES ({data});"
+        data = ', '.join(values)
+        sql_insert = f'INSERT INTO {target_schema}.{target_table_name} VALUES ({data});'
         target_cursor.execute(sql_insert)
-
 
 
 # Conexión a la base de datos de origen
@@ -147,34 +147,44 @@ target_conn = psycopg2.connect(
 )
 _target_cursor = target_conn.cursor()
 
-PREFIX = ""
+PREFIX = ''
 
 tables_cant = len(tables_to_migrate)
 
 try:
     for i, _source_table_name in enumerate(tables_to_migrate):
-        _target_table_name = f"{PREFIX}{_source_table_name}"
-        print(f"Migrando la tabla {_source_table_name}...")
+        _target_table_name = f'{PREFIX}{_source_table_name}'
+        print(f'Migrando la tabla {_source_table_name}...')
         progress = (i + 1) / tables_cant * 100
-        print(f"Procesando {i + 1}/{tables_cant} ({progress:.2f}%)", end="\r")
+        print(f'Procesando {i + 1}/{tables_cant} ({progress:.2f}%)', end='\r')
 
-        _target_cursor.execute("BEGIN;")
+        _target_cursor.execute('BEGIN;')
         migrate_table_structure(_source_cursor, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name)
-        migrate_constraints(_source_cursor, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name, PREFIX)
+        migrate_constraints(
+            _source_cursor, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name, PREFIX
+        )
         migrates_sequences(
-            _source_cursor, SOURCE_SCHEMA, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name, prefix=""
+            _source_cursor,
+            SOURCE_SCHEMA,
+            _source_table_name,
+            _target_cursor,
+            TARGET_SCHEMA,
+            _target_table_name,
+            prefix='',
         )
 
         # Comentado por el momento la importación de datos hasta tanto se resuelva el error con los distintos tipos recibidos
-        migrate_data(_source_cursor, SOURCE_SCHEMA, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name)
-        print(f"Tabla {_source_table_name} migrada correctamente.")
-        
-        #Hacemos un commit parcial por cada tabla
-        _target_cursor.execute("COMMIT;") 
+        migrate_data(
+            _source_cursor, SOURCE_SCHEMA, _source_table_name, _target_cursor, TARGET_SCHEMA, _target_table_name
+        )
+        print(f'Tabla {_source_table_name} migrada correctamente.')
+
+        # Hacemos un commit parcial por cada tabla
+        _target_cursor.execute('COMMIT;')
 
 except Exception as ex:
-    print(f"Error al migrar la tabla {_source_table_name}: {ex}")
-    _target_cursor.execute("ROLLBACK;")
+    print(f'Error al migrar la tabla {_source_table_name}: {ex}')
+    _target_cursor.execute('ROLLBACK;')
 
 # Cerramos las conexiones
 _source_cursor.close()
